81c2a070d1c441672636a05610d32d91b779c79b
rohehe <rohhe.he@gmail.com>
2020-05-19 21:58:45 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-UA-Compatible" content="IE=edge">
+	<title>.:: Halaman Login Admin ::.</title>
+	<link rel="stylesheet" href="stylee.css">
+	<link rel="stylesheet" type="text/css" href="../include/style_admin.css">
+	<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+</head>
+<body>
+	<div class="login">
+		<h2>From Login</h2>
+		<div class="box-login">
+			<i class="fas fa-user"></i>
+			<input type="text" placeholder="Username">
+		</div>
+
+		<div class="box-login">
+			<i class="fas fa-lock"></i>
+			<input type="password" placeholder="Password">
+		</div>	
+
+		<button type="submit" class="btn-login">
+			login
+		</button>
+
+		<div class="bottom">
+			<a href="">Register</a>
+			<a href="">Forget Password</a>
+		</div>
+
+	</div>
+
+	<!--<div id="header">
+		<center><h2>Form Login Admin Adat Ngarot</h2></center>
+		<br>
+		<br>
+	</div>
+	<center>
+	<div class="col-md-4 col-md-offset-4">
+	<form name="form1" method="post" action="cek_login.php?coba=wp">
+		<table class="table" align="center" border="0" cellpadding="0">
+			<tr>
+				<td><label>Username:</label></td>
+				<td><input class="form-control" name="id_user" type="text" id="id_user"></td>
+			</tr>
+			<tr>
+				<td><label>Password:</label></td>
+				<td><input class="form-control" name="password" type="password" id="password"></td>
+			</tr>
+			
+				<td colspan="2" align="center"><input class="btn btn-info" type="submit" name="submit" value="Login"></td>
+			</tr>
+		</table>
+	</form>
+	</div>
+	</center> -->
+
+</body>
+</html>
